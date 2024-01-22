b8cff06f2de0d0b075ff0824e674fc0d065dffca
davidleacock <dave.leacock@gmail.com>
2024-01-21 18:23:49 -0800

    add TODO to fix ExternalEvent

--- delivery-date-service/src/main/scala/deliverydate/ExternalEvent.scala
+++ delivery-date-service/src/main/scala/deliverydate/ExternalEvent.scala
@@ -2,4 +2,6 @@ package deliverydate
 
 import java.time.Instant
 
+// TODO, the ExternalEvent wont contain the deliveryDate. Its this systems job to calculate it.
+// Replace with processedTime or something
 case class ExternalEvent (packageId: String, eventId: Int, deliveryDate: Instant)
