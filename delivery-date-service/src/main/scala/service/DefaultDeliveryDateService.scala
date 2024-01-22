f88a0266e235c7f76811326a55af82d624bdc490
davidleacock <dave.leacock@gmail.com>
2024-01-21 17:54:25 -0800

    import TypeKey from entity, clean up imports. fix replyTo type

--- delivery-date-service/src/main/scala/service/DefaultDeliveryDateService.scala
+++ delivery-date-service/src/main/scala/service/DefaultDeliveryDateService.scala
@@ -1,16 +1,22 @@
 package service
 
 import akka.actor.typed.ActorSystem
-import akka.cluster.sharding.typed.scaladsl.{ClusterSharding, EntityTypeKey}
+import akka.cluster.sharding.typed.scaladsl.{ ClusterSharding, EntityTypeKey }
 import akka.util.Timeout
-import deliverydate.{DeliveryDateEntity}
-import deliverydate.DeliveryDateEntity.{DeliveryDate, GetDeliveryDate, UpdateDeliveryDate, UpdateSuccessful}
+import deliverydate.DeliveryDateEntity
+import deliverydate.DeliveryDateEntity.{
+  DeliveryDate,
+  GetDeliveryDate,
+  Reply,
+  UpdateDeliveryDate,
+  UpdateSuccessful
+}
 import org.slf4j.LoggerFactory
 
 import java.time.Instant
 import java.util.UUID
 import scala.concurrent.duration.DurationInt
-import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.{ ExecutionContext, Future }
 
 class DefaultDeliveryDateService(
   clusterSharding: ClusterSharding
@@ -22,29 +28,27 @@ class DefaultDeliveryDateService(
 
   private implicit val askTimeout: Timeout = Timeout(5.seconds)
 
-  // move to object?
-  val TypeKey: EntityTypeKey[DeliveryDateEntity.Command] =
-    EntityTypeKey[DeliveryDateEntity.Command]("DeliveryDate")
-
   override def upsertDeliveryDate(
     packageId: UUID,
     updatedDate: Instant
   ): Future[String] = {
     clusterSharding
-      .entityRefFor(TypeKey, packageId.toString)
-      .ask[UpdateSuccessful] { ref =>
+      .entityRefFor(DeliveryDateEntity.TypeKey, packageId.toString)
+      .ask[Reply] { ref =>
         UpdateDeliveryDate(packageId, updatedDate, ref)
-      }.map { reply =>
+      }
+      .map { reply =>
         reply.toString
       }
   }
 
   override def getDeliveryDate(packageId: UUID): Future[Option[Instant]] = {
     clusterSharding
-      .entityRefFor(TypeKey, packageId.toString)
+      .entityRefFor(DeliveryDateEntity.TypeKey, packageId.toString)
       .ask[DeliveryDate] { ref =>
         GetDeliveryDate(packageId, ref)
-      }.map { reply =>
+      }
+      .map { reply =>
         reply.deliveryDate
       }
   }
