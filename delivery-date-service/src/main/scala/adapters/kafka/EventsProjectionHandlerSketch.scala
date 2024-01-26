74ff49657173364affc418016d8a2dbb6ce217a8
davidleacock <dave.leacock@gmail.com>
2024-01-26 14:07:15 -0800

    write basic event -> kafka projection

--- delivery-date-service/src/main/scala/adapters/kafka/EventsProjectionHandlerSketch.scala
+++ delivery-date-service/src/main/scala/adapters/kafka/EventsProjectionHandlerSketch.scala
@@ -6,13 +6,31 @@ import akka.kafka.scaladsl.SendProducer
 import akka.projection.eventsourced.EventEnvelope
 import akka.projection.scaladsl.Handler
 import deliverydate.DeliveryDateEntity.Event
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.slf4j.LoggerFactory
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 class EventsProjectionHandlerSketch(
   system: ActorSystem[_],
   topic: String,
-  sendProducer: SendProducer[String, Array[Byte]])
+  sendProducer: SendProducer[String, String])
     extends Handler[EventEnvelope[Event]] {
-  override def process(envelope: EventEnvelope[Event]): Future[Done] = ???
+  private implicit val ec: ExecutionContext =
+    system.executionContext
+
+  private val log = LoggerFactory.getLogger(getClass)
+
+  override def process(envelope: EventEnvelope[Event]): Future[Done] = {
+
+    val event = envelope.event
+
+    // TODO What should the key be? How is that determined?
+    val producerRecord = new ProducerRecord(topic, "", s"Dummy Event: $event")
+
+    sendProducer.send(producerRecord).map { metadata =>
+      log.info(s"Published event to topic - metadata [$metadata")
+      Done
+    }
+  }
 }
