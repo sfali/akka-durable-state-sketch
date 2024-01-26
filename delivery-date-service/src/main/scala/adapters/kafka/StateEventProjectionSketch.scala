caf29a2d41e887a50f6124af1b9a5a9874fa1e24
davidleacock <dave.leacock@gmail.com>
2024-01-26 14:07:46 -0800

    create first pass at projection event journal to kafka publisher

--- delivery-date-service/src/main/scala/adapters/kafka/StateEventProjectionSketch.scala
+++ delivery-date-service/src/main/scala/adapters/kafka/StateEventProjectionSketch.scala
@@ -1,82 +1,65 @@
 package adapters.kafka
 
 import akka.actor.typed.ActorSystem
+import akka.kafka.ProducerSettings
+import akka.kafka.scaladsl.SendProducer
 import akka.persistence.jdbc.query.scaladsl.JdbcReadJournal
 import akka.persistence.query.Offset
 import akka.persistence.query.typed.{EventEnvelope => QueryEventEnvelope}
+import akka.projection.ProjectionId
 import akka.projection.eventsourced.scaladsl.EventSourcedProvider
 import akka.projection.scaladsl.SourceProvider
 import deliverydate.DeliveryDateEntity.Event
-import akka.kafka.ProducerSettings
-import akka.kafka.scaladsl.SendProducer
-import akka.projection.{Projection, ProjectionId, eventsourced}
-import org.apache.kafka.clients.producer.{KafkaProducer, ProducerRecord}
 import org.apache.kafka.common.serialization.StringSerializer
-import akka.stream.scaladsl.Source
-//import akka.projection.kafka.scaladsl.KafkaProducer
-import akka.projection.eventsourced.EventEnvelope
-import akka.projection.scaladsl.AtLeastOnceFlowProjection
-import akka.kafka.ProducerSettings
-import akka.kafka.scaladsl.SendProducer
-import akka.persistence.jdbc.query.scaladsl.JdbcReadJournal
-import akka.persistence.query.Offset
-import akka.projection.eventsourced.EventEnvelope
-import akka.projection.eventsourced.scaladsl.EventSourcedProvider
+import slick.jdbc.JdbcBackend.Database
 import akka.projection.jdbc.scaladsl.JdbcProjection
-import akka.projection.scaladsl.{ AtLeastOnceProjection, SourceProvider }
-import akka.projection.{ ProjectionBehavior, ProjectionId }
 
-class StateEventProjectionSketch(implicit val system: ActorSystem[_]) {
+// TODO This should be elsewhere its not an adapter
+object StateEventProjectionSketch {
 
-  private val numberOfSliceRanges: Int = 4
-  private val sliceRanges =
-    EventSourcedProvider.sliceRanges(
-      system,
-      JdbcReadJournal.Identifier,
-      numberOfSliceRanges
-    )
+  def startProjectionToKafka(database: Database)(implicit system: ActorSystem[_]) {
 
-  private val minSlice: Int = sliceRanges.head.min
-  private val maxSlice: Int = sliceRanges.head.max
-  // TODO is this right?
-  private val entityType: String = "DeliveryDateState"
+    val numberOfSliceRanges: Int = 4
 
+    val sliceRanges =
+      EventSourcedProvider.sliceRanges(
+        system,
+        JdbcReadJournal.Identifier,
+        numberOfSliceRanges
+      )
 
-  val kafkaTopic = "delivery-date-events"
+    val minSlice: Int = sliceRanges.head.min
+    val maxSlice: Int = sliceRanges.head.max
+    // TODO is this right?
+    val entityType: String = "DeliveryDateState"
 
-  val kafkaProducerSettings: ProducerSettings[String, String] =
-    ProducerSettings(system, new StringSerializer, new StringSerializer)
-      .withBootstrapServers("localhost:9092")
+    val topic = "delivery-date-events"
 
+    val sourceProvider
+    : SourceProvider[Offset, QueryEventEnvelope[Event]] = {
+      EventSourcedProvider.eventsBySlices[Event](
+        system,
+        JdbcReadJournal.Identifier,
+        entityType,
+        minSlice,
+        maxSlice
+      )
+    }
 
-  private val sourceProvider: SourceProvider[Offset, QueryEventEnvelope[Event]] = {
-    EventSourcedProvider.eventsBySlices[Event](
-      system,
-      JdbcReadJournal.Identifier,
-      entityType,
-      minSlice,
-      maxSlice
+    val sendProducer: SendProducer[String, String] = {
+      val producerSettings: ProducerSettings[String, String] =
+        ProducerSettings(system, new StringSerializer, new StringSerializer)
+          .withBootstrapServers("localhost:9092")
+
+      SendProducer(producerSettings)
+    }
+
+    JdbcProjection.atLeastOnceAsync(
+      projectionId = ProjectionId("DeliveryDateProjection", "DeliveryDate"),
+      sourceProvider,
+      handler =
+        () => new EventsProjectionHandlerSketch(system, topic, sendProducer),
+      sessionFactory = () => new SlickDbSession(database)
     )
   }
-
-  def createProducer: SendProducer[String, Array[Byte]] = ???
-
-
-  // TODO finish building handler
-  // TODO finish building sendProducer
-  // TODO bring in ScalikeJdbcSession (Wtf is that even)
-
-
-  JdbcProjection.exactlyOnce(
-    projectionId = ProjectionId("DeliveryDateProjection", "DeliveryDate"),
-    sourceProvider,
-    handler = () => ???,
-    sessionFactory = () => ???,
-  )
-
-
-
-
-
-
 }
