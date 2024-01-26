c1024621af2544d4775d4de78c6a6057d2d8f3ab
davidleacock <dave.leacock@gmail.com>
2024-01-26 14:06:56 -0800

    Create SlickDbSession to manage db sessions

---
+++ delivery-date-service/src/main/scala/adapters/kafka/SlickDbSession.scala
@@ -0,0 +1,30 @@
+package adapters.kafka
+
+import akka.japi.function.Function
+import akka.projection.jdbc.JdbcSession
+import slick.jdbc.JdbcBackend.Database
+import slick.jdbc.JdbcBackend.Session
+
+import java.sql.Connection
+
+// TODO Find other spot for this
+class SlickDbSession(db: Database) extends JdbcSession {
+  private var currentSession: Option[Session] = None
+
+  override def withConnection[Result](func: Function[Connection, Result]): Result = {
+    val session = db.createSession()
+    currentSession = Some(session)
+    try {
+      func.apply(session.conn)
+    } finally {
+      session.close()
+      currentSession = None
+    }
+  }
+
+  override def commit(): Unit = currentSession.foreach(_.conn.commit())
+
+  override def rollback(): Unit = currentSession.foreach(_.conn.rollback())
+
+  override def close(): Unit = currentSession.foreach(_.conn.close())
+}
