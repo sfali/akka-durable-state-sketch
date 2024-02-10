95ce6d2cd60baf606d71b74e75a46551868eb3d5
sfali <syed.f.ali@improving.com>
2024-02-10 12:34:41 -0500

    using changesBySlices

--- delivery-date-service/src/main/scala/projection/DomainProjection.scala
+++ delivery-date-service/src/main/scala/projection/DomainProjection.scala
@@ -3,12 +3,14 @@ package projection
 import akka.actor.typed.ActorSystem
 import akka.kafka.ProducerSettings
 import akka.kafka.scaladsl.SendProducer
-import akka.persistence.query.Offset
 import akka.persistence.query.typed.EventEnvelope
+import akka.persistence.query.{DurableStateChange, Offset}
 import akka.persistence.r2dbc.query.scaladsl.R2dbcReadJournal
+import akka.persistence.r2dbc.state.scaladsl.R2dbcDurableStateStore
 import akka.projection.eventsourced.scaladsl.EventSourcedProvider
 import akka.projection.r2dbc.scaladsl.R2dbcProjection
 import akka.projection.scaladsl.SourceProvider
+import akka.projection.state.scaladsl.DurableStateSourceProvider
 import akka.projection.{Projection, ProjectionId}
 import deliverydate.DeliveryDateEntity
 import org.apache.kafka.common.serialization.StringSerializer
@@ -19,44 +21,29 @@ object DomainProjection {
 
   def projection(
     sliceRange: Range
-  )(implicit system: ActorSystem[_]
+  )(implicit
+    system: ActorSystem[_]
   ): Projection[EventEnvelope[DeliveryDateEntity.Event]] = {
 
     val topic = system
       .settings
       .config
       .getString("delivery-date-service.projections.topic")
-    val entityType: String = "DeliveryDate"
 
     val minSlice = sliceRange.min
     val maxSlice = sliceRange.max
-    val projectionId =
-      ProjectionId("DomainEvents", s"domain-$minSlice-$maxSlice")
+    val projectionId = ProjectionId("DomainEvents", s"domain-$minSlice-$maxSlice")
 
     def sourceProvider(
       sliceRange: Range
-    ): SourceProvider[Offset, EventEnvelope[DeliveryDateEntity.Event]] = {
+    ): SourceProvider[Offset, EventEnvelope[DeliveryDateEntity.Event]] =
       EventSourcedProvider.eventsBySlices[DeliveryDateEntity.Event](
         system,
         readJournalPluginId = R2dbcReadJournal.Identifier,
-        entityType,
+        DeliveryDateEntity.EntityName,
         sliceRange.min,
         sliceRange.max
       )
-    }
-
-    def sendProducer(
-      minSlice: Int,
-      maxSlice: Int
-    ): SendProducer[String, String] = {
-      val producerSettings =
-        ProducerSettings(system, new StringSerializer, new StringSerializer)
-          .withBootstrapServers("localhost:9092")
-          .withClientId(s"egress-projection-$minSlice-$maxSlice")
-          .withCloseTimeout(0.seconds)
-
-      SendProducer(producerSettings)
-    }
 
     R2dbcProjection.atLeastOnceAsync(
       projectionId = projectionId,
@@ -66,8 +53,55 @@ object DomainProjection {
         new DomainEventsProjectionHandler(
           system,
           topic,
-          sendProducer(minSlice, maxSlice)
+          sendProducer(s"$minSlice-$maxSlice")
         )
     )
+  }
+
+  def changesBySlices(
+    sliceRange: Range
+  )(implicit
+    system: ActorSystem[_]
+  ): Projection[DurableStateChange[DeliveryDateEntity.DeliveryDateState]] = {
+    val topic = system
+      .settings
+      .config
+      .getString("delivery-date-service.projections.topic")
+
+    val sourceProvider =
+      DurableStateSourceProvider.changesBySlices[DeliveryDateEntity.DeliveryDateState](
+        system = system,
+        durableStateStoreQueryPluginId = R2dbcDurableStateStore.Identifier,
+        entityType = DeliveryDateEntity.EntityName,
+        minSlice = sliceRange.min,
+        maxSlice = sliceRange.max
+      )
+
+    val projectionKey = s"change-${sliceRange.min}-${sliceRange.max}"
+    R2dbcProjection.atLeastOnceAsync(
+      projectionId = ProjectionId("EgressProjection", projectionKey),
+      settings = None,
+      sourceProvider = sourceProvider,
+      handler = () =>
+        new EgressProjectionHandler(
+          system = system,
+          topic = topic,
+          sendProducer = sendProducer(projectionKey)
+        )
+    )
+  }
+
+  private def sendProducer(
+    suffix: String
+  )(implicit
+    system: ActorSystem[_]
+  ): SendProducer[String, String] = {
+    val producerSettings =
+      ProducerSettings(system, new StringSerializer, new StringSerializer)
+        .withBootstrapServers("localhost:9092")
+        .withClientId(s"egress-projection-$suffix")
+        .withCloseTimeout(0.seconds)
+
+    SendProducer(producerSettings)
   }
 }
